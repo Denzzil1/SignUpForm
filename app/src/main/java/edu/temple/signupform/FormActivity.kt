62c9bf89cbde3aa9507b5ae96149f0eb32aabf9a
Cory <denzzil123@gmail.com>
2022-02-10 03:31:25 -0500

    Set conditions for the log ins and also cleaned up how the program looks a bit.

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -2,10 +2,44 @@ package edu.temple.signupform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var name: TextView = findViewById(R.id.editText)
+        var email: TextView = findViewById(R.id.email)
+        var pass: TextView = findViewById(R.id.enterPassword)
+        var confirm: TextView = findViewById(R.id.confirmPassword)
+        var button:Button = findViewById(R.id.register)
+        var name2 = ""
+        button.setOnClickListener {
+        confirm.text.isEmpty()
+            if(name.text.isEmpty()){
+                name.setError("Please complete all fields")
+            }
+            if(email.text.isEmpty()){
+                    email.setError("Please complete all fields")
+                }
+            if(pass.text.isEmpty()){
+                pass.setError("Please complete all fields")
+            }
+            if(confirm.text.isEmpty()){
+                confirm.setError("Please complete all fields")
+            }
+            if(pass.text.toString().equals(confirm.text.toString())){
+
+            }
+            else{
+                Toast.makeText(this,"These passwords do not match",Toast.LENGTH_LONG).show()
+            }
+            name2 =  name.text.toString()
+            Toast.makeText(this,"Welcome $name2",Toast.LENGTH_LONG).show()
+
+        }
     }
 }
